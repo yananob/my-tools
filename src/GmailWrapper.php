31c1a15af2b1c72b55ae04aeff719bbf159dca8b
yananob <yananob@gmail.com>
2024-06-21 17:26:50 +0900

    gmailwrapper

--- src/GmailWrapper.php
+++ src/GmailWrapper.php
@@ -16,7 +16,7 @@ final class GmailWrapper
      * Returns an authorized API client.
      * @return \Google\Client the authorized client object
      */
-    public static function getClient()
+    public static function getClient(string $clientsecret_path, string $apitoken_path)
     {
         $logger = new Logger("GoogleAPI");
 
@@ -28,7 +28,7 @@ final class GmailWrapper
         $client = new \Google\Client();
         $client->setApplicationName('MyCFApp');
         $client->setScopes($SCOPES);
-        $client->setAuthConfig(dirname(__FILE__) . '/configs/googleapi_clientsecret.json');
+        $client->setAuthConfig($clientsecret_path);
         $client->setAccessType('offline');
         $client->setPrompt('select_account consent');
         // $client->setRedirectUri('https://www.google.com/');
@@ -38,10 +38,9 @@ final class GmailWrapper
         // created automatically when the authorization flow completes for the first
         // time.
         // https://sqripts.com/2022/08/25/20386/
-        $tokenPath = dirname(__FILE__) . '/configs/googleapi_token.json';
-        if (file_exists($tokenPath)) {
+        if (file_exists($apitoken_path)) {
             $logger->log("token file exists");
-            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $accessToken = json_decode(file_get_contents($apitoken_path), true);
             $client->setAccessToken($accessToken);
         }
 
@@ -57,10 +56,10 @@ final class GmailWrapper
                 throw new \Exception("THIS DOESN'T WORK");
             }
             // Save the token to a file.
-            if (!file_exists(dirname($tokenPath))) {
-                mkdir(dirname($tokenPath), 0700, true);
+            if (!file_exists(dirname($apitoken_path))) {
+                mkdir(dirname($apitoken_path), 0700, true);
             }
-            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+            file_put_contents($apitoken_path, json_encode($client->getAccessToken()));
         }
         return $client;
     }
