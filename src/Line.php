af8f2a2f007bd3b0d7e39539b5af419118bec538
yananob <yananob@gmail.com>
2024-11-16 13:14:35 +0900

    Switched to  LINE Messaging API

--- src/Line.php
+++ src/Line.php
@@ -7,39 +7,50 @@ namespace yananob\mytools;
 final class Line
 {
     private array $tokens;
+    private array $targetIds;
 
     public function __construct(string $configPath)
     {
         $config = Utils::getConfig($configPath);
         $this->tokens = $config["tokens"];
+        $this->targetIds = $config["target_ids"];
     }
 
-    public function sendMessage(string $target, string $message): void
+    public function sendMessage(string $bot, string $target, string $message): void
     {
-        if (!array_key_exists($target, $this->tokens)) {
+        if (!array_key_exists($bot, $this->tokens)) {
+            throw new \Exception("Unknown bot: {$bot}");
+        }
+        if (!array_key_exists($target, $this->targetIds)) {
             throw new \Exception("Unknown target: {$target}");
         }
 
         $headers = [
-            "contentType: application/x-www-form-urlencoded",
-            "Authorization: Bearer {$this->tokens[$target]}",
+            "Content-Type: application/json",
+            "Authorization: Bearer {$this->tokens[$bot]}",
         ];
-        $fields = [
-            "message" => $message,
+        $body = [
+            "to" => $this->targetIds[$target],
+            "messages" => [
+                [
+                    "type" => "text",
+                    "text" => $message,
+                ],
+            ],
         ];
         $ch = curl_init();
         try {
             curl_setopt_array($ch, [
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_URL => "https://notify-api.line.me/api/notify",
+                CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
                 CURLOPT_POST => true,
                 CURLOPT_HTTPHEADER => $headers,
-                CURLOPT_POSTFIELDS => $fields,
+                CURLOPT_POSTFIELDS => json_encode($body),
             ]);
             $response = curl_exec($ch);
             $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
             if ($httpcode != "200") {
-                throw new \Exception("Failed to send message [to: {$target}, message: {$message}]. Http response code: [{$httpcode}]");
+                throw new \Exception("Failed to send message [bot: {$bot}, to: {$target}, message: {$message}]. Http response code: [{$httpcode}]");
             }
         } finally {
             curl_close($ch);
