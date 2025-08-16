b86d3261e61050a2331d06e729ec6bac0b2779fb
yananob <yananob@gmail.com>
2025-08-16 15:14:58 +0900

    Gpt: chat/completionパラメーターで、追加のパラメーターを渡せるように Fixes #16

--- tests/GptTest.php
+++ tests/GptTest.php
@@ -7,11 +7,28 @@ use yananob\MyTools\Gpt;
 
 class GptTest extends TestCase
 {
-    public function testGetAnswer(): void
+    public function test_GetAnswer追加パラメーターなし(): void
     {
         $gpt = new Gpt("dummy_api_key", "gpt-4.1");
 
-        $answer = $gpt->getAnswer("あなたは日本のコメディアンです。", "自己紹介をしてください。");
+        $answer = $gpt->getAnswer(
+            "あなたは日本のコメディアンです。",
+            "自己紹介をしてください。",
+        );
+        $this->assertNotEmpty($answer);
+    }
+
+    public function test_GetAnswer追加パラメーターあり(): void
+    {
+        $gpt = new Gpt("dummy_api_key", "gpt-4.1");
+
+        $answer = $gpt->getAnswer(
+            "あなたは日本のコメディアンです。",
+            "自己紹介をしてください。",
+            [
+                "reasoning_effort" => "minimal"
+            ],
+        );
         $this->assertNotEmpty($answer);
     }
 }
